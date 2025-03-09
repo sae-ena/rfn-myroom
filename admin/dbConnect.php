a981288c6c02003625136aa7f0840a11fe2039ea
sae-ena <sareenawal@gmail.com>
2025-03-09 23:03:07 +0545

    #Docker

--- admin/dbConnect.php
+++ admin/dbConnect.php
@@ -1,21 +1,10 @@
 <?php
-
-$connectToServer = true;
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-
-if(isset($connectToServer) && $connectToServer){
     $host = "mysql-29620eed-kidssujal-9bd8.j.aivencloud.com"; 
     $port = 18250; // your MySQL port 
     $username = "avnadmin"; // your MySQL username
     $password = "AVNS_1N9Dr_M5lJZIRxcd8gj"; 
     $dbname = "rf_db"; 
-}else{
-    $host = "127.0.0.1"; 
-    $port = 3306;
-    $username = "root"; 
-    $password = "admin"; 
-    $dbname = "rommfinderDB"; 
-}
+
 
     $conn = new mysqli($host, $username, $password, $dbname, $port);
 
